b765bd4c0e31333aa56234614e61dc6c3931d456
Dyego Alves <dyego.santos.alves@gmail.com>
2023-03-06 03:03:06 -0300

    feat(início): Add content page início
        - Add Slides show photos;
        - Add lists images;
        - Clear content início 'index'
        - Logo move for folder imagens
    
    @dyegoalves

---
+++ src/assets/imagens/imgslides.js
@@ -0,0 +1,17 @@
+import img1 from "./img1.jpg";
+import img2 from "./img2.jpg";
+import img3 from "./img3.jpg";
+import img8 from "./img8.jpg";
+import img31 from "./img31.jpg";
+import img41 from "./img41.jpg";
+
+const images = {
+  img1,
+  img2,
+  img3,
+  img8,
+  img31,
+  img41,
+};
+
+export default images;
